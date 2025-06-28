4c7c014820beeea6870378d4c1b91975f5e5a399
Staff Communication Add-in <addon@company.com>
2025-06-28 12:13:03 -0400

    Fix webpack configuration and add production manifest
    - Remove babel-loader dependency for simpler deployment
    - Add production-ready manifest.xml with proper URLs
    - Successful build verification completed
    - Ready for immediate Git deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,22 +8,12 @@ module.exports = {
     commands: './src/commands/commands.js'
   },
   output: {
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
     clean: true
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
@@ -43,15 +34,24 @@ module.exports = {
       template: './src/commands/commands.html',
       filename: 'commands.html',
       chunks: ['commands']
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'manifest.xml',
+          to: 'manifest.xml'
+        }
+      ]
     })
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'dist')
     },
     compress: true,
     port: 3000,
     https: true,
+    hot: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
@@ -59,7 +59,6 @@ module.exports = {
     }
   },
   resolve: {
-    extensions: ['.js', '.json']
+    extensions: ['.js', '.jsx', '.json']
   }
 };
-
